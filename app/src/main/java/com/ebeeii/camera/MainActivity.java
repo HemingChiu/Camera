faa6fb7b04b5b2eef69d6e4db050a5640fe9da09
HM\HiM <6917697@qq.com>
2016-10-26 01:48:46 +0800

    add SlideMenu

--- app/src/main/java/com/ebeeii/camera/MainActivity.java
+++ app/src/main/java/com/ebeeii/camera/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
